9c2dff5288a44572b2c155c22292502004b4e335
Apachek12 <apachecom11@gmail.com>
2026-06-02 12:11:27 +0200

    Update SecurityConfig.java

--- src/main/java/com/TFG/TendaSoft/config/SecurityConfig.java
+++ src/main/java/com/TFG/TendaSoft/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 import java.util.List;
 
